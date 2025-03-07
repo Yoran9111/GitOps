950a0d8db20544d68c119d18c45db45f86829f66
Yoran9111 <Yoranvuranok@gmail.com>
2025-03-07 16:46:22 +0100

    Update flux.nix

--- Nixos/Roles/flux.nix
+++ Nixos/Roles/flux.nix
@@ -1,36 +1,20 @@
 { config, lib, pkgs, ... }:
 
-# Define Flux options and systemd service inside the config block
 {
-  options.services.flux = lib.mkOption {
-    type = lib.types.attrs;
-    description = "Flux configuration options.";
-    default = {
-      enable = false;
-      branch = "main";
-      gitRepository = "https://github.com/Yoran9111/GitOps.git";
-      secretRef = null;
-    };
+  # Define the Flux GitOps service in NixOS configuration
+  services.flux = {
+    enable = true;  # Enable the Flux service
+    branch = "main";  # Specify the Flux branch you want to use
+    gitRepository = "https://github.com/Yoran9111/GitOps.git";  # The Git repository for Flux
+    secretRef = null;  # If you have any secret configuration, set it here
   };
 
-  # Flux systemd service should go inside the config block
-  config = {
-    # Enabling Flux and defining its parameters
-    services.flux = {
-      enable = true;
-      branch = "main";
-      gitRepository = "https://github.com/Yoran9111/GitOps.git";
-      secretRef = null;
-    };
-
-    # Configure the systemd service for Flux
-    systemd.services.flux = {
-      description = "Flux GitOps Tool";
-      wantedBy = [ "multi-user.target" ];
-
-      serviceConfig.ExecStart = "${pkgs.flux}/bin/flux";
-      serviceConfig.ExecReload = "${pkgs.flux}/bin/flux";
-      serviceConfig.Restart = "always";
-    };
+  # Enable systemd service for flux (this is automatically handled)
+  systemd.services.flux = {
+    description = "Flux GitOps Tool";
+    wantedBy = [ "multi-user.target" ];
+    serviceConfig.ExecStart = "${pkgs.flux}/bin/flux";
+    serviceConfig.ExecReload = "${pkgs.flux}/bin/flux";
+    serviceConfig.Restart = "always";
   };
 }
