559f09ef222ea8810b48e209646deee1e9ba8029
Yoran9111 <Yoranvuranok@gmail.com>
2025-03-07 16:47:05 +0100

    Update flux.nix

--- Nixos/Roles/flux.nix
+++ Nixos/Roles/flux.nix
@@ -1,15 +1,20 @@
 { config, lib, pkgs, ... }:
 
 {
-  # Define the Flux GitOps service in NixOS configuration
+  imports = [
+    # Import your flux configuration here
+    /home/jip/lib/nixos/Nixos/Roles/flux.nix
+  ];
+
+  # Now you can use the services.flux option because it's imported
   services.flux = {
-    enable = true;  # Enable the Flux service
-    branch = "main";  # Specify the Flux branch you want to use
-    gitRepository = "https://github.com/Yoran9111/GitOps.git";  # The Git repository for Flux
-    secretRef = null;  # If you have any secret configuration, set it here
+    enable = true;
+    branch = "main";
+    gitRepository = "https://github.com/Yoran9111/GitOps.git";
+    secretRef = null;
   };
 
-  # Enable systemd service for flux (this is automatically handled)
+  # If you want to configure flux with a systemd service:
   systemd.services.flux = {
     description = "Flux GitOps Tool";
     wantedBy = [ "multi-user.target" ];
