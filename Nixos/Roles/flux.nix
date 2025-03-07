180efbf590c836230fefe72f2ea85de076a29309
Yoran9111 <Yoranvuranok@gmail.com>
2025-03-07 16:50:07 +0100

    Update flux.nix

--- Nixos/Roles/flux.nix
+++ Nixos/Roles/flux.nix
@@ -1,19 +1,16 @@
 { config, lib, pkgs, ... }:
 
 {
-  # Minimal Flux configuration
   services.flux = {
     enable = true;
-    branch = "main";  # Specify the branch (default is "main")
-    gitRepository = "https://github.com/Yoran9111/GitOps.git";  # Your GitOps repo
-    secretRef = null;  # Optional secret reference
+    branch = "main";
+    gitRepository = "https://github.com/Yoran9111/GitOps.git";
+    secretRef = null;
   };
 
-  # Simplified systemd service for Flux
   systemd.services.flux = {
     description = "Flux GitOps Tool";
     wantedBy = [ "multi-user.target" ];
-
     serviceConfig.ExecStart = "${pkgs.flux}/bin/flux";
     serviceConfig.ExecReload = "${pkgs.flux}/bin/flux";
     serviceConfig.Restart = "always";
