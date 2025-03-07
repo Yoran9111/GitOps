2afa0bdfd484409f08df00fe38f6f8b5e7ba7d85
Yoran9111 <Yoranvuranok@gmail.com>
2025-03-07 16:35:10 +0100

    Update flux.nix

--- Nixos/Roles/flux.nix
+++ Nixos/Roles/flux.nix
@@ -1,14 +1,25 @@
-{ config, pkgs, ... }: {
-  # Ensure Flux is installed
-  environment.systemPackages = with pkgs; [
-    flux  # This ensures flux is installed as a package
-  ];
+{ config, lib, pkgs, ... }:
 
-  # Configure Flux as a system service
-  services.flux = {
-    enable = true;
-    gitRepository = "https://github.com/Yoran9111/GitOps.git";  # Update to your repository URL
-    branch = "main";  # Ensure Flux tracks the correct branch
-    secretRef = null;  # If using SSH keys, you can define this
+{
+  options = {
+    services.flux = lib.mkOption {
+      type = lib.types.attrs;
+      description = "Flux configuration options.";
+      default = {
+        enable = false;
+        branch = "main";
+        gitRepository = "https://github.com/Yoran9111/GitOps.git";
+        secretRef = null;
+      };
+    };
+  };
+
+  config = {
+    services.flux = {
+      enable = true;
+      branch = "main";
+      gitRepository = "https://github.com/Yoran9111/GitOps.git";
+      secretRef = null;
+    };
   };
 }
