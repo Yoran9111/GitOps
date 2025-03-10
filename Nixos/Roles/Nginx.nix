d77a8379dce3812d381c55e362bc9e34d377b6ab
Yoran9111 <Yoranvuranok@gmail.com>
2025-03-10 09:45:24 +0100

    Update Nginx.nix

--- Nixos/Roles/Nginx.nix
+++ Nixos/Roles/Nginx.nix
@@ -8,20 +8,20 @@
   ];
 
   services.nginx = {
-    enable = true;
-    virtualHosts = {
-      "mywebsite.com" = {
-        root = "/nix/store/79dljmcihdrv2bcrgp1imms81akxh599-nginx-1.26.3/html";
-        locations = {
-          "/" = {
-            proxyPass = "http://192.168.254.134:30080/";
-            extraConfig = ''
-              index index.html;
-              proxy_set_header Host $host;
-              proxy_set_header X-Real-IP $remote_addr;
-              proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-            '';
-          };
+  enable = true;
+  virtualHosts = {
+    "mywebsite.com" = {
+      listen = [ "8080" ];  # Nginx will listen on port 8080
+      root = "/nix/store/79dljmcihdrv2bcrgp1imms81akxh599-nginx-1.26.3/html";
+      locations = {
+        "/" = {
+          proxyPass = "http://192.168.254.134:30080/";
+          extraConfig = ''
+            index index.html;
+            proxy_set_header Host $host;
+            proxy_set_header X-Real-IP $remote_addr;
+            proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+          '';
         };
       };
     };
