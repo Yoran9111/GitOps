5feef3bd17855a4e96f06cafb3d4afcf9d1a5392
Yoran9111 <Yoranvuranok@gmail.com>
2025-03-10 10:12:20 +0100

    Update apps.nix

--- Nixos/Roles/apps.nix
+++ Nixos/Roles/apps.nix
@@ -1,10 +1,15 @@
-{ config, pkgs, ... }:
+{ config, pkgs, ... }: 
 
 {
+  # Add system packages to install on NixOS
   environment.systemPackages = with pkgs; [
-    fluxcd
-    git
-    jdk11
-    curl
+    fluxcd           # Flux CD (for GitOps)
+    git              # Git for version control
+    jdk11            # Java 11 (or you can use jdk17 or jdk21 if needed)
+    curl             # Curl for making network requests
+    gitlab-runner    # GitLab Runner for CI/CD pipelines
+    openssh-client   # SSH client (for secure communication)
+    vim              # A text editor (if you prefer Vim)
+    wget             # A tool for downloading files from the web
   ];
 }
