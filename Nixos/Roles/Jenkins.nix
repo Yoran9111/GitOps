9468a5f5ea189e83922cfd7c1fb44a0aebd7ac48
Yoran9111 <Yoranvuranok@gmail.com>
2025-03-10 09:31:31 +0100

    Update Jenkins.nix

--- Nixos/Roles/Jenkins.nix
+++ Nixos/Roles/Jenkins.nix
@@ -3,7 +3,7 @@
 { 
 services.jenkins = {
   enable = true;
-  port = 8080;  # Change this if needed
+  port = 8079;  # Change this if needed
   user = "jenkins";  # Runs as jenkins user
   home = "/var/lib/jenkins";  # Jenkins home directory
   javaPackage = pkgs.jdk11;  # Java version required by Jenkins
