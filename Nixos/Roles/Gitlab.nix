7d478e14618cd6bb17c9abc084b8669c4d453af0
Yoran9111 <Yoranvuranok@gmail.com>
2025-03-10 10:22:18 +0100

    Update Gitlab.nix

--- Nixos/Roles/Gitlab.nix
+++ Nixos/Roles/Gitlab.nix
@@ -1,43 +1,25 @@
 { config, pkgs, lib, ... }:
 
 {
-  # Import the necessary GitLab Runner module
-  imports =
-    [ 
-      # Import the GitLab Runner service module
-      <nixos/modules/services/continuous-integration/gitlab-runner.nix>
-    ];
+  environment.systemPackages = with pkgs; [
+    gitlab-runner
+    # Add other packages here
+  ];
 
-  # Enable and configure GitLab Runner service
-  services.gitlabRunner = {
-    enable = true;
-    token = "<your-gitlab-runner-token>";  # Replace with your GitLab Runner registration token
-    executor = "shell";  # Shell executor type, you can change to docker if needed
-    runners = [
-      {
-        name = "nixos-runner";
-        url = "https://gitlab.com/";  # GitLab instance URL
-        token = "<your-gitlab-token>";  # Replace with your GitLab registration token
-        description = "GitLab Runner for NixOS";
-        tags = [ "nixos" ];  # Runner tags
-        run_untagged = true;  # Allow untagged jobs to run
-        locked = false;  # Allow to use the runner for any project
-      }
-    ];
+  # Define GitLab Runner service as a systemd service
+  systemd.services.gitlab-runner = {
+    description = "GitLab Runner";
+    wantedBy = [ "multi-user.target" ];
+    after = [ "network.target" ];
+
+    serviceConfig.ExecStart = "${pkgs.gitlab-runner}/bin/gitlab-runner run";
+    serviceConfig.ExecReload = "${pkgs.gitlab-runner}/bin/gitlab-runner restart";
+    serviceConfig.ExecStop = "${pkgs.gitlab-runner}/bin/gitlab-runner stop";
   };
 
-  # Define the gitlab-runner user and group
+  # Ensure GitLab Runner runs as a specific user (optional)
   users.users.gitlab-runner = {
     isSystemUser = true;
-    shell = pkgs.zsh;  # You can set the shell to zsh
-    group = "gitlab-runner";  # Define the group for gitlab-runner
+    shell = pkgs.bashInteractive;
   };
-
-  # Create the gitlab-runner group
-  users.groups.gitlab-runner = {};
-
-  # Enable zsh globally on the system
-  programs.zsh.enable = true;
-
-  # Other settings and options
 }
